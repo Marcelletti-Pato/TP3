54cde31bc6bf0808ad1bceaafb68ec1abf15c422
DanielMarcelletti <danimarche32@gmail.com>
2015-04-27 03:48:37 -0300

    Cambio de define a const

--- FahrenheitACelcius.c
+++ FahrenheitACelcius.c
@@ -7,14 +7,16 @@
 
 #include <stdio.h>
 
-#define LOWER 0 /* límite inferior de la tabla */
-#define UPPER 300 /* límite superior */
-#define STEP 20 /* tamaño del incremento */
+	const int LOWER = 0; /* límite inferior de la tabla */
+	const int UPPER = 300; /* límite superior */
+	const int STEP = 20; /* tamaño del incremento */
 
-main (void)
+int main (void)
 {
 	int fahr;
 	printf("Grados\tFahrenheit\n");
 	for (fahr = LOWER ;fahr <= UPPER ;fahr = fahr + STEP)
 		printf("%3d \t%6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+
+return 0;
 }
